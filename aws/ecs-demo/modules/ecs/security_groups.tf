e2fec6088cca0d9ed98a2745b316807afa17267f
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2020-10-21 11:40:39 +0300

    Remove duplicate ingress/egress

--- aws/ecs-demo/modules/ecs/security_groups.tf
+++ aws/ecs-demo/modules/ecs/security_groups.tf
@@ -3,13 +3,6 @@ resource "aws_security_group" "lb" {
   name        = "Load balancer"
   description = "Load balancer security group"
   vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 # Allow traffic from public internet
@@ -42,20 +35,6 @@ resource "aws_security_group" "backend" {
   name        = "Backend"
   description = "Backend security group"
   vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
-
-  ingress {
-    from_port       = var.backend_port
-    to_port         = var.backend_port
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 # Allow traffic from the load balancer to the backends
