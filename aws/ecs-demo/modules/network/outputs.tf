7cb490ce6f3fc66b2fb231ab7408968eebf0d129
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2020-10-23 14:53:46 +0300

    Add outputs for subnet ids

--- aws/ecs-demo/modules/network/outputs.tf
+++ aws/ecs-demo/modules/network/outputs.tf
@@ -1,3 +1,11 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+output "pubic_subnet_ids" {
+  value = aws_subnet.public.*.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private.*.id
+}
