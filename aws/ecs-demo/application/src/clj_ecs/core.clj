7ff82c9ab948dc721d88d19b0338f64231fd8257
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2020-10-23 22:30:51 +0300

    Add database connection to application

--- aws/ecs-demo/application/src/clj_ecs/core.clj
+++ aws/ecs-demo/application/src/clj_ecs/core.clj
@@ -1,19 +1,29 @@
 (ns clj-ecs.core
   (:require [ring.adapter.jetty :as jetty]
             [timbre-json-appender.core :as tas]
-            [taoensso.timbre :as timbre]))
+            [taoensso.timbre :as timbre]
+            [hikari-cp.core :as hikari]
+            [next.jdbc :as jdbc]))
 
-(defn handler [version {:keys [uri]}]
-  (timbre/info :uri uri :git-sha version)
+(defn handler [db version {:keys [uri]}]
+  (timbre/info :uri uri :version version)
   {:status  200
    :headers {"Content-Type" "text/plain"}
    :body    (str "Hello World!"
                  (when version
-                   (str " Running at " version)))})
+                   (str " Running at " version "."))
+                 (format " Database has %d connections" (:count (jdbc/execute-one! db ["select count(*) as count from pg_stat_activity"]))))})
 
 (defn -main [& _]
   (tas/install)
-  (jetty/run-jetty (partial handler (System/getenv "IMAGE_TAG"))
-                   {:port (or (some-> (System/getenv "PORT")
-                                      (Integer/parseInt))
-                              4000)}))
+  (let [datasource (hikari/make-datasource {:username      (System/getenv "DB_USER")
+                                            :password      (System/getenv "DB_PASSWORD")
+                                            :database-name (System/getenv "DB_NAME")
+                                            :server-name   (System/getenv "DB_HOST")
+                                            :port-number   (Integer/parseInt (System/getenv "DB_PORT"))
+                                            :pool-name     "db-pool"
+                                            :adapter       "postgresql"})]
+    (jetty/run-jetty (partial handler datasource (System/getenv "IMAGE_TAG"))
+                     {:port (or (some-> (System/getenv "PORT")
+                                        (Integer/parseInt))
+                                4000)})))
