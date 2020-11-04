e641c6a0a6c38e2154ec671ac107c602e2fb97c9
Kari Marttila <kari.marttila@iki.fi>
2020-11-04 14:31:59 +0200

    Fixed default tags in ecr module

--- aws/ecs-demo/modules/ecr/main.tf
+++ aws/ecs-demo/modules/ecr/main.tf
@@ -1,12 +1,23 @@
 locals {
   workspace_name = terraform.workspace == "default" ? "" : "-${terraform.workspace}"
-  prefix_name    = "${var.prefix}${local.workspace_name}"
+  module_name      = "bastion"
+  default_tags     = {
+    Prefix    = var.prefix
+    Workspace = terraform.workspace
+    Module    = local.module_name
+    Terraform = "true"
+  }
 }
 
 resource "aws_ecr_repository" "backend" {
-  name = "${local.prefix_name}-backend"
+  name = "${var.prefix}${local.workspace_name}-backend"
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
+
+  tags = merge(local.default_tags, {
+    Name = "${var.prefix}${local.workspace_name}-backend"
+  })
+
 }
