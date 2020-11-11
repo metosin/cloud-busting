d3278353110f7a8bed4b9fe9c3405d84c220a49a
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2020-11-11 15:08:04 +0200

    Login to ECR in app build script

--- aws/ecs-demo/application/build.sh
+++ aws/ecs-demo/application/build.sh
@@ -6,6 +6,9 @@ pushd ../modules/ecr > /dev/null
 ECR_REPO_URL=$(terraform output backend_repository_url)
 popd > /dev/null
 
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --out text)
+aws ecr get-login-password | docker login --username AWS --password-stdin https://$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+
 echo $ECR_REPO_URL
 
 GIT_SHA=$(git rev-parse --short HEAD)
