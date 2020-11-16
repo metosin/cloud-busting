c3c066a0c9f4d6b9aa2e897d2012f54aed4aeb41
Kimmo Koskinen <kimmo.koskinen@metosin.fi>
2020-11-16 15:53:14 +0200

    Run terraform fmt

--- aws/ecs-demo/modules/rds/monitoring.tf
+++ aws/ecs-demo/modules/rds/monitoring.tf
@@ -8,15 +8,13 @@ resource "aws_cloudwatch_metric_alarm" "rds_disk_queue_depth_alarm" {
   statistic           = "Maximum"
   threshold           = "5"
   # Should be defined in some project related parameter file.
-  alarm_description   = "Monitors RDS disk queue depth"
-  dimensions          = {
+  alarm_description = "Monitors RDS disk queue depth"
+  dimensions = {
     DBInstanceIdentifier = aws_db_instance.database.identifier
   }
 
-  alarm_actions = [
-    data.terraform_remote_state.common.outputs.monitoring_sns_topic_arn]
-  ok_actions    = [
-    data.terraform_remote_state.common.outputs.monitoring_sns_topic_arn]
+  alarm_actions = [data.terraform_remote_state.common.outputs.monitoring_sns_topic_arn]
+  ok_actions    = [data.terraform_remote_state.common.outputs.monitoring_sns_topic_arn]
 
   tags = merge(local.default_tags, {
     Name = "${local.res_prefix}-rds-disk-queue-depth-alarm"
